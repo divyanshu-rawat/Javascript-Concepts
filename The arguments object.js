c19401ca4ba4af4fa37dda1ca97af9afb634ed0a
divyanshu <divyanshu.r46956@gmail.com>
2017-01-04 01:11:06 +0530

    #Lesson 7

---
+++ The arguments object.js
@@ -0,0 +1,77 @@
+
+// The arguments Object
+
+// Every function scope in JavaScript can access the special variable arguments. This variable holds a list of all the arguments that were passed to the
+// function.
+
+var x = 2,y = 4;
+
+function arguments(x,y){
+
+	// console.log(arguments);
+	// console.log(Array.prototype.slice.call(arguments));
+
+	// Because this conversion is slow, it is not recommended to use it in performance-critical sections of code.
+};
+
+arguments(x,y);
+
+// The arguments object is not an Array. While it has some of the semantics of an array - namely the length property - it does not inherit from 
+// Array.prototype and is in fact an Object.
+
+// Due to this, it is not possible to use standard array methods like push, pop or slice on arguments. While iteration with a plain for loop works just fine
+// it is necessary to convert it to a real Array in order to use the standard Array methods on it.
+
+// Converting to an Array
+
+// The code below will return a new Array containing all the elements of the arguments object.
+
+
+// Passing Arguments
+
+// The following is the recommended way of passing arguments from one function to another.
+
+function foo() {
+
+    bar.apply(null, arguments);
+}
+
+foo(x,y);
+
+
+function bar(a, b) {
+    
+    // console.log(arguments);
+}
+
+
+
+
+// Formal Parameters and Arguments Indices
+
+// The arguments object creates getter and setter functions for both its properties, as well as the function's formal parameters.
+
+// As a result, changing the value of a formal parameter will also change the value of the corresponding property on the arguments object, and the other way
+// around.
+
+function boo(a, b, c) {
+
+	console.log(arguments);
+
+    arguments[0] = 2;
+
+    console.log(arguments);
+
+}
+
+boo(1, 2, 3);
+
+
+
+
+
+
+
+
+
+
