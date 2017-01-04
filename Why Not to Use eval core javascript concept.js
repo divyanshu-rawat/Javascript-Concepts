8262870a74754b43383cbbd215e101d7c7fb33d1
divyanshu <divyanshu.r46956@gmail.com>
2017-01-05 01:47:11 +0530

    #Lesson 16

---
+++ Why Not to Use eval core javascript concept.js
@@ -0,0 +1,95 @@
+// The eval function will execute a string of JavaScript code in the local scope.
+
+
+// The eval function will execute a string of JavaScript code in the local scope.
+
+// var number = 1;
+
+// function test() {
+
+//     var number = 2;
+
+//     eval('number = 3');
+
+//     return number;
+
+// }
+
+
+var number = 1;
+function test() {
+    var number = 2;
+    
+    var copyOfEval = eval;
+
+    copyOfEval('number = 3');
+    
+    return number;
+}
+test(); // 2
+
+console.log(number); // 3
+
+// test(); // 3
+
+// console.log(number);
+
+
+
+// The use of eval should be avoided. 99.9% of its "uses" can be achieved without it.
+
+
+// eval in Disguise
+
+// The timeout functions setTimeout and setInterval can both take a string as their first argument. This string will always get executed in the global scope 
+// since eval is not being called directly in that case.
+
+// Security Issues
+
+// eval also is a security problem, because it executes any code given to it. It should never be used with strings of unknown or untrusted origins.
+
+// In Conclusion
+
+// eval should never be used. Any code that makes use of it should be questioned in its workings, performance and security. If something requires eval in 
+// order to work, it should not be used in the first place. A better design should be used, that does not require the use of eval.
+
+
+
+// The language also defines a global variable that has the value of undefined; this variable is also called undefined. However, this variable is neither a 
+// constant nor a keyword of the language. This means that its value can be easily overwritten.
+
+
+// ES5 Note: undefined in ECMAScript 5 is no longer writable in strict mode, but its name can still be shadowed by for example a function with the name 
+// undefined.
+
+
+// Here are some examples of when the value undefined is returned:
+
+// Accessing the (unmodified) global variable undefined.
+
+// Accessing a declared but not yet initialized variable.
+
+// Implicit returns of functions due to missing return statements.
+
+// return statements that do not explicitly return anything.
+
+// Lookups of non-existent properties.
+
+// Function parameters that do not have any explicit value passed.
+
+// Anything that has been set to the value of undefined.
+
+// Any expression in the form of void(expression)
+
+
+
+
+
+
+
+
+
+
+
+
+
