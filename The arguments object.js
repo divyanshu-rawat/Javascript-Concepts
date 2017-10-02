da652e0b1599f32d52dc0e60f97c1bb7e57edc87
wabray17 <wabray17@gmail.com>
2017-10-02 12:13:40 -0500

    Added details about converting to an array to access array methods - #4

--- The arguments object.js
+++ The arguments object.js
@@ -8,10 +8,10 @@ var x = 2,y = 4;
 
 function arguments(x,y){
 
-	// console.log(arguments);
-	// console.log(Array.prototype.slice.call(arguments));
+  // console.log(arguments); 
+  // console.log(Array.prototype.slice.call(arguments));
 
-	// Because this conversion is slow, it is not recommended to use it in performance-critical sections of code.
+  // Because this conversion is slow, it is not recommended to use it in performance-critical sections of code.
 };
 
 arguments(x,y);
@@ -22,10 +22,29 @@ arguments(x,y);
 // Due to this, it is not possible to use standard array methods like push, pop or slice on arguments. While iteration with a plain for loop works just fine
 // it is necessary to convert it to a real Array in order to use the standard Array methods on it.
 
+function baz(c, d) {
+  console.log(arguments); // { 0: 1, 1: 2 }
+  for(var i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]); // 1, 2
+  }
+
+  args.forEach(arg => console.log(arg)); // TypeErrror
+}
+
+baz(1, 2);
+
 // Converting to an Array
 
-// The code below will return a new Array containing all the elements of the arguments object.
+// The code below will return a new Array containing all the elements of the arguments object. You then have access to all typical 
+// Array methods. 
 
+function baz(c, d) {
+  var args = [].slice.call(arguments);
+  console.log(args); // [1, 2]
+  args.forEach(arg => console.log(arg)); // 1, 2
+}
+
+baz(1, 2); 
 
 // Passing Arguments
 
@@ -56,7 +75,7 @@ function bar(a, b) {
 
 function boo(a, b, c) {
 
-	console.log(arguments);
+  console.log(arguments);
 
     arguments[0] = 2;
 
